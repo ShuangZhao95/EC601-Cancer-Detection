b272f2aa5b553adb69b057c1ae8cac1f27e39eab
John_Curci <jcurci92@bu.edu>
2017-12-02 21:40:20 -0500

    reorganize directories

---
+++ Modeling/ex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#$ -P ece601
+#$ -o /project/ece601/cancer_detection/Code/output.txt
+#$ -m e jcurci92@gmail.com
+
+
+module load python/3.6.2
+module load tensorflow/r1.3_cpu
+module load nltk
+module unload python
+module load python/3.6_intel-2018.1.023
+
+python Doc2Vec.py
---
+++ Modeling/readme.md
@@ -0,0 +1,3 @@
+All the data are here: (https://www.kaggle.com/c/msk-redefining-cancer-treatment/data)
+
+Just download training_text, training_variants, test_text and test_variants.
